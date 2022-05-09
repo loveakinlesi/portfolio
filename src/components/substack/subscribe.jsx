b76db5bc9b25315dce3ea9bae2515323e7230368
Love <akinlesil@gmail.com>
2022-05-09 23:50:45 +0100

    Added substack components

---
+++ src/components/substack/subscribe.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const SubscribeComponent = () => {
+  return (
+    <iframe className=" w-100 h-100 mx-auto"
+    title="Subscribe"
+      src="https://loveakinlesi.substack.com/embed"
+      style={{border: 'none', background: '#131414', color: '#e2e2e2', width: '100%'}}
+      frameBorder="0"
+      scrolling="no"
+    ></iframe>
+  );
+};
+
